ac2bef68f08c06b36224b4abf59f5754c24fe0e8
Omer Celebi <116555725+OmerCeleb@users.noreply.github.com>
2024-02-12 10:34:07 +0100

    Update Day07_AutoComplate.java

--- seleniumproject/src/test/java/com/myfirstproject/Day07_AutoComplate.java
+++ seleniumproject/src/test/java/com/myfirstproject/Day07_AutoComplate.java
@@ -15,17 +15,17 @@ public class Day07_AutoComplate extends TestBase {
 
         // When user type "uni" in the search box
         driver.findElement(By.id("myCountry")).sendKeys("uni");
-        Thread.sleep(1000);
+        Thread.sleep(100);
         // And select the United Kingdom from the suggestion
-        driver.findElement(By.xpath("//div[@div='myCountryautoComplete-list']//div[.='United Kingdom']")).click();
-        Thread.sleep(1000);
+        driver.findElement(By.xpath("//*[@id='myCountryautocomplete-list']/div[2]")).click();
+        Thread.sleep(100);
 
         // And click on submit button
-        driver.findElement(By.xpath("//input[@type='button'")).click();
+        driver.findElement(By.xpath("/html/body/main/div/div[1]/div/form/input")).click();
         Thread.sleep(1000);
 
         // Then verify the result contains United Kingdom
-        Assertions.assertTrue(driver.findElement(By.id("result")).isSelected());
+        Assertions.assertTrue(driver.findElement(By.id("result")).isDisplayed());
 
 
 
